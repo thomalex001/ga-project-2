f179a07261162ee0013582c78372b1631f2e7dec
Joel <joelsahiti@yahoo.co.uk>
2022-12-01 11:51:54 +0000

    Home-second-attempt

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+const Home = () => (
+  <section className="hero is-fullheight-with-navbar is-success">
+    <div className="hero-body">
+      <div className="container">
+        <p className="title is-1 has-text-centered has-text-black">
+          <span className="logo-emoji"></span> Rick & Morty
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default Home;

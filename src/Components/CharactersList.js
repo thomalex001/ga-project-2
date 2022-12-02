08526e8fe9aa07dce4bfbb231216b68a731efa68
Joel <joelsahiti@yahoo.co.uk>
2022-12-02 09:47:04 +0000

    p

--- src/Components/CharactersList.js
+++ src/Components/CharactersList.js
@@ -21,15 +21,8 @@ const CharactersList = () => {
           {characters.map((character) => (
             <CharactersCard
               key={character.id}
-<<<<<<< HEAD
               name={character.name}
               image={character.image}
-=======
-              id={character.id}
-              name={character.name}
-              image={character.image}
-              
->>>>>>> fc3b2c23d38196383004d46540e9be5ea3aa932e
             />
           ))}
         </div>

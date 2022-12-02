6c669de9bf16aec95662d74c1ba4a2b36021c127
Joel <joelsahiti@yahoo.co.uk>
2022-12-02 15:34:40 +0000

    Working page arrows

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import CharactersList from './Components/CharactersList';
-import Pagination from './Components/Pagination2';
+import CharacterBio from './Components/CharacterBio';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/list/:id" element={<CharacterBio />} />
         <Route path="/list" element={<CharactersList />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<p>This is not a valid route</p>} />
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-
 const BASE_URL = 'https://rickandmortyapi.com/api/character';
 
-export const getAllCharacters = () => axios.get(`${BASE_URL}`);
+export const getAllCharacters = (pageNumber) =>
+  axios.get(`${BASE_URL}/?page=${pageNumber}`);
+
 export const getCharacterBio = (characterId) =>
   axios.get(`${BASE_URL}/${characterId}`);
